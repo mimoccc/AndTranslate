cfc2d8776ac9925ea2ef99e0ce15120c2efb9874
Magnus Olsson <magnus72@gmail.com>
2012-01-05 03:22:00 +0100

    init

---
+++ AndTranslate/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Text;
+using System.Xml;
+using AndTranslate.net.bing.api;
+
+namespace AndTranslate
+{
+    //  
+    class Program
+    {
+
+        private static string infile = "";
+        private static string outfile = "";
+        private static string fromLang = "";
+        private static string toLang = "";
+
+        static XmlDocument xdoc = new XmlDocument();
+        NameValueCollection nv = new NameValueCollection();
+        private Thread th;
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                infile = args[0];
+                fromLang = args[1];
+                toLang = args[2];
+                outfile = "translatedto-" + toLang + "-" + infile;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error reading arguments");
+               return;
+            }
+                
+            Program p = new Program();
+    //     string result =    Translator.Translate("en", "sv", "In the Settings you can define which lap times you want to time. To prepare for a new race, select distance and press Start. Instructions will be given under the race on where to press the Time Mark button.");
+        }
+
+        public Program()
+        {
+            
+            // read infile
+            xdoc.Load(infile);
+            XmlNode root = xdoc.ChildNodes.Item(1);
+
+          
+            foreach (XmlElement node in root.ChildNodes)
+            {
+                nv.Add(node.GetAttribute("name"), node.InnerText);
+            }
+
+            doTranslation();
+            
+        }
+
+
+        private void doTranslation()
+        {
+
+          th = new Thread(new ThreadStart(sendRequest));
+            th.Start();
+        }
+
+        private void sendRequest()
+        {
+
+            foreach (string key in nv.AllKeys)
+            {
+                string totrans = nv.Get(key);
+                string transl = Translator.Translate(fromLang, toLang, totrans);
+                nv.Set(key, transl);
+                Thread.Sleep(300);
+            }
+            WriteOutput((fromLang), nv);
+        }
+
+        private static void WriteOutput(string lang, NameValueCollection values)
+        {
+            XmlDocument doc = new XmlDocument();
+            //Xml Declaration
+            XmlDeclaration declaration = doc.CreateXmlDeclaration("1.0", "utf-8", "yes");
+            //Attach declaration to the document
+            doc.AppendChild(declaration);
+
+            //Create root element
+            XmlElement root = doc.CreateElement("resources");
+
+            //Attach the root node to the document
+            doc.AppendChild(root);
+
+            foreach (string key in values.AllKeys)
+            {
+                XmlElement str = doc.CreateElement("string");
+                //Add an attribute name with value John Smith
+                str.SetAttribute("name", key);
+                str.InnerText = values.Get(key);
+                root.AppendChild(str);
+            }
+            doc.Save(outfile);
+        }
+ 
+    }
+}
---
+++ AndTranslate/Translator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using AndTranslate.net.bing.api;
+
+namespace AndTranslate
+{
+
+    class Translator
+    {
+        private static string APP_ID = "A416E9E5DFE936E4EAD0CDEFEE44885B7B2276AC";
+
+
+        public static string Translate(string fromLang, string toLang, string query)
+        {
+            // BingService implements IDisposable.
+            using (BingService service = new BingService())
+            {
+                try
+                {
+                    SearchRequest request = BuildRequest(fromLang, toLang, query);
+
+                    // Send the request; display the response.
+                    SearchResponse response = service.Search(request);
+                  return   ParseTranslation(response);
+                }
+                catch (System.Web.Services.Protocols.SoapException ex)
+                {
+                    // A SOAP Exception was thrown. Display error details.
+                    DisplayErrors(ex.Detail);
+                }
+                catch (System.Net.WebException ex)
+                {
+                    // An exception occurred while accessing the network.
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return "|ERROR - no translation";
+
+        }
+
+                static SearchRequest BuildRequest(string  fromLang, string toLang, string query)
+        {
+            SearchRequest request = new SearchRequest();
+
+            // Common request fields (required)
+            request.AppId = APP_ID;
+            request.Query =query;
+            request.Sources = new SourceType[] { SourceType.Translation };
+
+            // SourceType-specific fields (required)
+            request.Translation = new TranslationRequest();
+            request.Translation.SourceLanguage = fromLang;
+            request.Translation.TargetLanguage = toLang;
+
+            // Common request fields (optional)
+            request.Version = "2.2";
+
+            return request;
+        }
+
+
+                static string ParseTranslation(SearchResponse response)
+        {
+            // Display the results header.
+            //Console.WriteLine("Bing API Version " + response.Version);
+            //Console.WriteLine("Translation results for " + response.Query.SearchTerms);
+            //Console.WriteLine();
+
+            // Display the Translation results.
+            //foreach (TranslationResult result in response.Translation.Results)
+            //{
+            //    Console.WriteLine(result.TranslatedTerm);
+            //}
+            if (response.Translation != null)
+            {
+                return response.Translation.Results.First().TranslatedTerm;
+            }
+            return "|ERROR - no translation|";
+        }
+
+
+
+        static void DisplayErrors(XmlNode errorDetails)
+        {
+            // Add the default namespace to the namespace manager.
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(
+                errorDetails.OwnerDocument.NameTable);
+            nsmgr.AddNamespace(
+                "api",
+                "http://schemas.microsoft.com/LiveSearch/2008/03/Search");
+
+            XmlNodeList errors = errorDetails.SelectNodes(
+                "./api:Errors/api:Error",
+                nsmgr);
+
+            if (errors != null)
+            {
+                // Iterate over the list of errors and display error details.
+                Console.WriteLine("Errors:");
+                Console.WriteLine();
+                foreach (XmlNode error in errors)
+                {
+                    foreach (XmlNode detail in error.ChildNodes)
+                    {
+                        Console.WriteLine(detail.Name + ": " + detail.InnerText);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
